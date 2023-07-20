fa33c7cdc3759ab51b4e0e056c0f09195a3282d9
Isaac Diaz <isaac.diaz.lopez@hotmail.com>
2023-07-20 17:08:47 +0200

    Image, Formulario, StateMonedas, consultaAPI, StyleComponents, CustomHooks

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: 'EUR', nombre: 'Euro'},
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'GBP', nombre: 'Libra Esterlina'},
+    { id: 'MXC', nombre: 'Peso Mexicano'},
+]
+
+export {
+    monedas
+}
